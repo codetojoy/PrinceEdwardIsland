843d400cc65489ac86f3eaf6de5425003130bdfe
Michael Easter <codetojoy@gmail.com>
2021-09-24 08:58:06 -0300

    incremental update Fri 24 Sep 2021 08:58:06 ADT

---
+++ refresh-zodiac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e 
+
+SRC_DIR=../../PrinceEdwardIsland/pei-poli-zodiac
+DEST_DIR=./web/pei-poli-zodiac
+
+stat $SRC_DIR/zodiac.json > /dev/null 2>&1
+stat $SRC_DIR/viz/index.html > /dev/null 2>&1
+
+cp $SRC_DIR/zodiac.json $DEST_DIR/.
+cp $SRC_DIR/viz/index.html $DEST_DIR/.
