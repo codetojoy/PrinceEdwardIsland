12841e9c1eeb24977011e6e4f47198ab0aa9c7f8
Michael Easter <codetojoy@gmail.com>
2021-09-24 08:57:04 -0300

    incremental update Fri 24 Sep 2021 08:57:04 ADT

--- pei-poli-zodiac/src/json_generator/run.sh
+++ pei-poli-zodiac/src/json_generator/run.sh
@@ -3,12 +3,13 @@
 set -e
 
 stat ../../zodiac.csv > /dev/null 2>&1
-cp ../../zodiac.csv .
+# cp ../../zodiac.csv .
+
 ./gradlew clean installDist
 stat staging/bin/json_generator > /dev/null 2>&1
 
-./staging/bin/json_generator zodiac.csv zodiac.json
+./staging/bin/json_generator ../../zodiac.csv ../../zodiac.json
 
-cat zodiac.json
+cp ../../zodiac.json ../../viz/.
 
 echo "Ready."
