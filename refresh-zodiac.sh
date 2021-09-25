8c674814c10d44174edfed6c219d9e0f120b5293
Michael Easter <codetojoy@gmail.com>
2021-09-25 10:52:36 -0300

    extracted JS, CSS

--- refresh-zodiac.sh
+++ refresh-zodiac.sh
@@ -7,6 +7,10 @@ DEST_DIR=./web/pei-poli-zodiac
 
 stat $SRC_DIR/zodiac.json > /dev/null 2>&1
 stat $SRC_DIR/viz/index.html > /dev/null 2>&1
+stat $SRC_DIR/viz/zodiac.css > /dev/null 2>&1
+stat $SRC_DIR/viz/app.js > /dev/null 2>&1
 
 cp $SRC_DIR/zodiac.json $DEST_DIR/.
 cp $SRC_DIR/viz/index.html $DEST_DIR/.
+cp $SRC_DIR/viz/zodiac.css $DEST_DIR/.
+cp $SRC_DIR/viz/app.js $DEST_DIR/.
