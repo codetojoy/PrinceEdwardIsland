d59be4b99369db5812e6785a6fc12de7a3c656a9
Michael Easter <codetojoy@gmail.com>
2021-10-23 14:06:35 -0300

    French, new look

--- refresh-zodiac.sh
+++ refresh-zodiac.sh
@@ -10,14 +10,23 @@ DEST_DIR=./web/pei-poli-zodiac
 stat $SRC_DIR/zodiac.json > /dev/null 2>&1
 stat $SRC_DIR/zodiac_elements.json > /dev/null 2>&1
 
+stat $SRC_DIR/zodiac_fr.json > /dev/null 2>&1
+stat $SRC_DIR/zodiac_elements_fr.json > /dev/null 2>&1
+
 stat $SRC_DIR/viz/index.html > /dev/null 2>&1
+stat $SRC_DIR/viz/index_fr.html > /dev/null 2>&1
+
 stat $SRC_DIR/viz/zodiac.css > /dev/null 2>&1
+
 stat $SRC_DIR/viz/app.js > /dev/null 2>&1
+stat $SRC_DIR/viz/const.js > /dev/null 2>&1
 
 stat $SRC_DIR/viz/info.html > /dev/null 2>&1
+stat $SRC_DIR/viz/info_fr.html > /dev/null 2>&1
 stat $SRC_DIR/viz/info.css > /dev/null 2>&1
 
 stat $SRC_DIR/viz/other.html > /dev/null 2>&1
+stat $SRC_DIR/viz/other_fr.html > /dev/null 2>&1
 
 stat $SRC_DIR/viz/nav.css > /dev/null 2>&1
 stat $SRC_DIR/viz/nav.js > /dev/null 2>&1
@@ -27,14 +36,23 @@ stat $SRC_DIR/viz/nav.js > /dev/null 2>&1
 cp $SRC_DIR/zodiac.json $DEST_DIR/.
 cp $SRC_DIR/zodiac_elements.json $DEST_DIR/.
 
+cp $SRC_DIR/zodiac_fr.json $DEST_DIR/.
+cp $SRC_DIR/zodiac_elements_fr.json $DEST_DIR/.
+
 cp $SRC_DIR/viz/index.html $DEST_DIR/.
+cp $SRC_DIR/viz/index_fr.html $DEST_DIR/.
+
 cp $SRC_DIR/viz/zodiac.css $DEST_DIR/.
+
 cp $SRC_DIR/viz/app.js $DEST_DIR/.
+cp $SRC_DIR/viz/const.js $DEST_DIR/.
 
 cp $SRC_DIR/viz/info.html $DEST_DIR/.
+cp $SRC_DIR/viz/info_fr.html $DEST_DIR/.
 cp $SRC_DIR/viz/info.css $DEST_DIR/.
 
 cp $SRC_DIR/viz/other.html $DEST_DIR/.
+cp $SRC_DIR/viz/other_fr.html $DEST_DIR/.
 
 cp $SRC_DIR/viz/nav.css $DEST_DIR/.
 cp $SRC_DIR/viz/nav.js $DEST_DIR/.
--- web/pei-poli-zodiac/app.js
+++ web/pei-poli-zodiac/app.js
@@ -1,46 +1,15 @@
-// ----------
+import * as c from "./const.js";
 
-const NORMAL_JSON_FILE = "./zodiac.json";
-const ELEMENTS_JSON_FILE = "./zodiac_elements.json";
-const BACKGROUND_LIGHT = "hsl(61,80%,80%)";
-const BACKGROUND_DARK = "hsl(80,30%,40%)";
-const BACKGROUND_RANGE = [BACKGROUND_LIGHT, BACKGROUND_DARK];
-
-const GREEN_PARTY = "Green";
-const LIBERAL_PARTY = "Liberal";
-const NDP_PARTY = "NDP";
-const PC_PARTY = "PC";
-const UNKNOWN_PARTY = "Unknown";
-
-const NUM_SIBLINGS_FOR_SMALL_TEXT = 4;
-const NUM_CHARS_FOR_TINY_TEXT = 14;
-
-const MODE_NORMAL = "normal";
-const MODE_ELEMENTS = "elements";
-
-// colours sampled from party websites
-const PARTY_COLOR_MAP = {
-  [GREEN_PARTY]: d3.rgb(64, 157, 74),
-  [LIBERAL_PARTY]: d3.rgb(224, 31, 43),
-  [NDP_PARTY]: d3.rgb(243, 50, 48),
-  [PC_PARTY]: d3.rgb(46, 133, 196),
-  [UNKNOWN_PARTY]: d3.color("white"),
-};
-
-const color = d3.scaleLinear().domain([-1, 5]).range(BACKGROUND_RANGE).interpolate(d3.interpolateHcl);
-
-// ----------
-
-function getFillColor(d) {
-  return d.children ? color(d.depth) : PARTY_COLOR_MAP[d.data.party];
+export function getFillColor(d) {
+  return d.children ? c.color(d.depth) : c.PARTY_COLOR_MAP[d.data.party];
 }
 
-function hasManyChildren(d) {
+export function hasManyChildren(d) {
   let result = false;
   const isLeaf = d.data.children == null;
   if (isLeaf && d.parent && d.parent.data && d.parent.data.children) {
     const numNodes = d.parent.data.children.length;
-    result = numNodes >= NUM_SIBLINGS_FOR_SMALL_TEXT;
+    result = numNodes >= c.NUM_SIBLINGS_FOR_SMALL_TEXT;
   }
   return result;
 }
@@ -49,7 +18,7 @@ function getTextClass(d) {
   let result = "label";
 
   if (hasManyChildren(d)) {
-    if (d.data.name && d.data.name.length >= NUM_CHARS_FOR_TINY_TEXT) {
+    if (d.data.name && d.data.name.length >= c.NUM_CHARS_FOR_TINY_TEXT) {
       result = "label-tiny";
     } else {
       result = "label-small";
@@ -59,14 +28,44 @@ function getTextClass(d) {
   return result;
 }
 
+var myLocation = {
+  getCurrentURL: function () {
+    return window.location.href;
+  },
+};
+
+function isFrenchMode() {
+  const fileName = myLocation.getCurrentURL().split("/").slice(-1);
+  const isFrench = fileName == c.FRENCH_HTML_FILE;
+  return isFrench;
+}
+
+function getLocalizedJsonFile(jsonFile) {
+  let result = jsonFile;
+  if (isFrenchMode()) {
+    switch (jsonFile) {
+      case c.NORMAL_JSON_FILE:
+        result = c.NORMAL_JSON_FR_FILE;
+        break;
+      case c.ELEMENTS_JSON_FILE:
+        result = c.ELEMENTS_JSON_FR_FILE;
+        break;
+      default:
+        result = jsonFile;
+        break;
+    }
+  }
+  return result;
+}
+
 // ----------
 
-function updateNormalMode() {
-  drawCircle(NORMAL_JSON_FILE);
+export function updateNormalMode() {
+  drawCircle(c.NORMAL_JSON_FILE);
 }
 
 function updateElementsMode() {
-  drawCircle(ELEMENTS_JSON_FILE);
+  drawCircle(c.ELEMENTS_JSON_FILE);
 }
 
 function drawCircle(jsonFile) {
@@ -84,7 +83,7 @@ function drawCircle(jsonFile) {
     .size([diameter - margin, diameter - margin])
     .padding(2);
 
-  d3.json(jsonFile, function (error, root) {
+  d3.json(getLocalizedJsonFile(jsonFile), function (error, root) {
     if (error) throw error;
 
     root = d3
@@ -98,7 +97,7 @@ function drawCircle(jsonFile) {
 
     let focus = root,
       nodes = pack(root).descendants(),
-      view;
+      currentView;
 
     let circle = g
       .selectAll("circle")
@@ -131,12 +130,21 @@ function drawCircle(jsonFile) {
 
     let node = g.selectAll("circle,text");
 
-    svg.style("background", color(-1)).on("click", function () {
+    svg.style("background", c.color(-1)).on("click", function () {
       zoom(root);
     });
 
     zoomTo([root.x, root.y, root.r * 2 + margin]);
 
+    // called when user clicks on a circle
+    // sets up a tween animation where `zoomTo` is called repeatedly
+    // focus - target
+    // view
+    //
+    // https://observablehq.com/@d3/d3-interpolatezoom
+    // - interpolation uses start, end view
+    // - `view` is defined by x, y, width
+    //    - x,y is center
     function zoom(d) {
       focus = d;
 
@@ -144,7 +152,9 @@ function drawCircle(jsonFile) {
         .transition()
         .duration(d3.event.altKey ? 7500 : 750)
         .tween("zoom", function (d) {
-          let i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+          const targetView = [focus.x, focus.y, focus.r * 2 + margin];
+          const i = d3.interpolateZoom(currentView, targetView);
+          // let i = d3.interpolateZoom(currentView, [focus.x, focus.y, focus.r * 2 + margin]);
           return function (t) {
             zoomTo(i(t));
           };
@@ -166,9 +176,15 @@ function drawCircle(jsonFile) {
         });
     }
 
+    // v is a view: [node.x, node.y, width]
+    // side-effects:
+    //    each node (i.e. circle, text selection) is translated
+    //    currentView is updated
+    //    circle selection: radius scaled
+    // see link above about interpolationZoom
     function zoomTo(v) {
       const k = diameter / v[2];
-      view = v;
+      currentView = v;
       node.attr("transform", function (d) {
         const x = (d.x - v[0]) * k;
         const y = (d.y - v[1]) * k;
@@ -185,38 +201,18 @@ function drawCircle(jsonFile) {
 
 function modeCheckboxHandler(event) {
   const value = event.target.value;
-  if (value === MODE_NORMAL) {
+  if (value === c.MODE_NORMAL) {
     updateNormalMode();
   } else {
     updateElementsMode();
   }
 }
 
-document.getElementById("checkbox-normal").addEventListener("change", modeCheckboxHandler);
-document.getElementById("checkbox-elements").addEventListener("change", modeCheckboxHandler);
-
-/*
-const backdrop = document.querySelector(".backdrop");
-const toggleButton = document.querySelector(".toggle-button");
-const sideNav = document.querySelector(".side-nav");
-
-toggleButton.addEventListener("click", function () {
-  sideNav.classList.add("open");
-  backdrop.style.display = "block";
-  setTimeout(function () {
-    backdrop.classList.add("open");
-  }, 10);
-});
-
-backdrop.addEventListener("click", function () {
-  sideNav.classList.remove("open");
-  closeModal();
-});
-
-function closeModal() {
-  backdrop.classList.remove("open");
-  setTimeout(function () {
-    backdrop.style.display = "none";
-  }, 200);
+if (document) {
+  if (document.getElementById("checkbox-normal")) {
+    document.getElementById("checkbox-normal").addEventListener("change", modeCheckboxHandler);
+  }
+  if (document.getElementById("checkbox-elements")) {
+    document.getElementById("checkbox-elements").addEventListener("change", modeCheckboxHandler);
+  }
 }
-*/
